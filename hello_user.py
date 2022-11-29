0d3152480a80aac26b2f18c28c424927a789f792
hap-py-python <117331957+hap-py-python@users.noreply.github.com>
2022-11-28 19:31:01 -0500

    Add files via upload

---
+++ hello_user.py
@@ -0,0 +1,9 @@
+from tkinter import *
+from tkinter import ttk
+
+
+root =Tk()
+
+Label(root, text='Hello User!\n\t"I have no special talent. I am only passionately curious."\t\n- Albert Einstein').pack()
+
+root.mainloop()
